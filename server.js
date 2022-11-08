1f02f85ff1a0a1f88bc4d735d62ff916404c886d
Rafael Vilas Boas <rafaelpiresvb@gmail.com>
2022-11-07 22:23:09 -0300

    feat: add preload

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
-import Nullstack from 'nullstack';
-import Application from './src/Application';
+import Nullstack from 'nullstack'
+import Application from './src/Application'
 
-const context = Nullstack.start(Application);
+const context = Nullstack.start(Application)
 
-context.start = async function start() {
-  // https://nullstack.app/pt-br/inicializacao-da-aplicacao
+function cacheFirstGenerationData() {
+  const pokeList = []
+  for (let i = 1; i < 152; i++) {
+    pokeList.push(`https://pokeapi.co/api/v2/pokemon/${i}`)
+  }
+  return pokeList
 }
 
-export default context;
+function cacheFirstGenerationSprites() {
+  const pokeSprites = []
+  for (let i = 1; i < 152; i++) {
+    pokeSprites.push(
+      `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i}.png`,
+    )
+  }
+  return pokeSprites
+}
+
+context.start = async function start() {
+  // https://nullstack.app/application-startup
+  const { worker } = context
+  worker.preload = [
+    ...cacheFirstGenerationData(),
+    ...cacheFirstGenerationSprites(),
+  ]
+}
+
+export default context
--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -2,7 +2,7 @@ export default function PokeCard({ pokeData }) {
   return (
     <figure class="flex h-36 w-28 flex-col overflow-hidden rounded-lg border border-grass">
       <span class="w-full p-2 text-right text-xs">#001</span>
-      <img src={pokeData} alt="" class="aspect-square w-20 self-center" />
+      <img src={pokeData} alt="" class="aspect-square w-20 self-center" loading="lazy" />
       <figcaption class=" mt-auto w-full bg-grass p-1 text-center text-sm capitalize text-white">
         Bulbasaur
       </figcaption>
